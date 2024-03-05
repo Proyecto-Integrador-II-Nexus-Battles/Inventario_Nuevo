347d0a3db1879f59184be5ba3b0f4d696aed081d
SrSwamp <dani.dm856@gmail.com>
2024-03-05 07:11:13 -0500

    se agregaron la uri de mongo atlas para poder conectarse via web desde cualquier dispositivo, los enpoints de get, post, get by id, put and delete, en estado de pruebas, al momenoto de usar es necesario instalar mongoose, express y nodemon

---
+++ Index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require("mongoose");
+require("dotenv").config();
+const heroRoutes = require("./routes/hero");
+
+const app = express();
+const port =  process.env.PORT || 9000;
+
+//middleware
+app.use(express.json());
+app.use(heroRoutes);
+
+
+//mongodb connection
+mongoose
+.connect(process.env.MONGODB_URI)
+.then(() => console.log('conected to Mongodb atlas'))
+.catch((error => console.error(error))); 
+
+app.listen(port, () => console.log('server listering on port', port));
