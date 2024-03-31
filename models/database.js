1d311b1b27538d145f089b79ff61c49b4b265770
David Cárdenas <santito500@live.com>
2024-03-31 17:03:09 -0500

    Update database.js

--- models/database.js
+++ models/database.js
@@ -1,15 +1,22 @@
-import mongoose from 'mongoose'
-import { armorSchema, epicasSchema, heroesSchema, itemsSchema, weaponsSchema } from '../schemas/cards.js'
-import { miBancoSchema } from '../schemas/bank.js'
-import { MONGO_URI, uri } from '../config.js'
+import mongoose from "mongoose";
+import {
+  armorSchema,
+  epicasSchema,
+  heroesSchema,
+  itemsSchema,
+  weaponsSchema,
+} from "../schemas/cards.js";
+import { miBancoSchema } from "../schemas/bank.js";
+import { MONGO_URI } from "../config.js";
 
-mongoose.connect(uri)
-  .then(() => console.log('Conectado a MONGODB'))
-  .catch((error) => console.error(error))
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("Conectado a MONGODB"))
+  .catch((error) => console.error(error));
 
-export const heroes = mongoose.model('cards_heroes', heroesSchema)
-export const armors = mongoose.model('cards_armors', armorSchema)
-export const items = mongoose.model('cards_items', itemsSchema)
-export const epics = mongoose.model('cards_epics', epicasSchema)
-export const weapons = mongoose.model('cards_weapons', weaponsSchema)
-export const mibanco = mongoose.model('miBanco', miBancoSchema)
+export const heroes = mongoose.model("cards_heroes", heroesSchema);
+export const armors = mongoose.model("cards_armors", armorSchema);
+export const items = mongoose.model("cards_items", itemsSchema);
+export const epics = mongoose.model("cards_epics", epicasSchema);
+export const weapons = mongoose.model("cards_weapons", weaponsSchema);
+export const mibanco = mongoose.model("miBanco", miBancoSchema);
