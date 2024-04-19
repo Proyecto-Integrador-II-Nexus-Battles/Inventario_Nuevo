c00aaccdcb58c30158b0d3a28f22195ee00fa249
David Cárdenas <santito500@live.com>
2024-04-18 23:45:34 -0500

    fix: :ambulance: Se eliminó una línea
    Se eliminó una línea que no exportaba los routers, por lo que se volvió a agregar y ya funciona correctamente.

--- routes/routes.js
+++ routes/routes.js
@@ -4,20 +4,22 @@ import {
   creditosController,
 } from "../controllers/controller.js";
 
+export const cardsRoutes = Router();
+
 // ? <--INVENTARIO-->
-cardsRoutes.get('/getAllCards', cardsController.getAll) // Devuelve todas las catas //*(requerido en inventario)
-cardsRoutes.get('/getEcommerceCard', cardsController.getEcommerceCard) // Devuelve todas las catas en venta con sus respectivos pecios //*(requerido en Ecommerce)
-cardsRoutes.patch('/modifyCard', cardsController.modifyCard)
+cardsRoutes.get("/getAllCards", cardsController.getAll); // Devuelve todas las catas //*(requerido en inventario)
+cardsRoutes.get("/getEcommerceCard", cardsController.getEcommerceCard); // Devuelve todas las catas en venta con sus respectivos pecios //*(requerido en Ecommerce)
+cardsRoutes.patch("/modifyCard", cardsController.modifyCard);
 
 // ?  <--MI BANCO-->
-cardsRoutes.get('/getEcommerceCard/:id', cardsController.getEcommerceCard)
-cardsRoutes.get('/cards', cardsController.filterCards)
-cardsRoutes.post('/getCardsByIDs', cardsController.getAll) // Devuelve los atibutos de las catas por los IDs ingresados //*(Requerido en miBanco y carritoDeCompra)
-cardsRoutes.post('/add-cards', cardsController.addBankCard) // Agrega cartas a la base de datos de invetario //*(requerido en miBanco, carritoCompras y subasta)
-cardsRoutes.post('/getBankCards', cardsController.getBankCard) // Devuelve todas las cartas del usuario //*(requerido en miBanco)
-cardsRoutes.post('/getCardsbyID', cardsController.getCardsbyID)
-cardsRoutes.post('/deckCard', cardsController.addDeckCard)
-cardsRoutes.delete('/delete-card', cardsController.deleteBankCard) // Elimina las cartas de mi banco
+cardsRoutes.get("/getEcommerceCard/:id", cardsController.getEcommerceCard);
+cardsRoutes.get("/cards", cardsController.filterCards);
+cardsRoutes.post("/getCardsByIDs", cardsController.getAll); // Devuelve los atibutos de las catas por los IDs ingresados //*(Requerido en miBanco y carritoDeCompra)
+cardsRoutes.post("/add-cards", cardsController.addBankCard); // Agrega cartas a la base de datos de invetario //*(requerido en miBanco, carritoCompras y subasta)
+cardsRoutes.post("/getBankCards", cardsController.getBankCard); // Devuelve todas las cartas del usuario //*(requerido en miBanco)
+cardsRoutes.post("/getCardsbyID", cardsController.getCardsbyID);
+cardsRoutes.post("/deckCard", cardsController.addDeckCard);
+cardsRoutes.delete("/delete-card", cardsController.deleteBankCard); // Elimina las cartas de mi banco
 
 // ?  <--CREDITOS-->
 cardsRoutes.post("/add-creditos", creditosController.addCredits);
