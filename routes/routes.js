ba22b8f029402c220913b69ca3b0066ffddc766e
David Cárdenas <santito500@live.com>
2024-04-24 07:24:42 -0500

    Cambios en models

--- routes/routes.js
+++ routes/routes.js
@@ -14,6 +14,7 @@ cardsRoutes.patch("/modifyCard", cardsController.modifyCard);
 // ?  <--MI BANCO-->
 cardsRoutes.get("/getEcommerceCard/:id", cardsController.getEcommerceCard);
 cardsRoutes.get("/cards", cardsController.filterCards);
+cardsRoutes.get("/get/cards/cart");
 cardsRoutes.post("/getCardsByIDs", cardsController.getAll); // Devuelve los atibutos de las catas por los IDs ingresados //*(Requerido en miBanco y carritoDeCompra)
 cardsRoutes.post("/add-cards", cardsController.addBankCard); // Agrega cartas a la base de datos de invetario //*(requerido en miBanco, carritoCompras y subasta)
 cardsRoutes.post("/getBankCards", cardsController.getBankCard); // Devuelve todas las cartas del usuario //*(requerido en miBanco)
