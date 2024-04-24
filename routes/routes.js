337a66bd5778cbd4521b3d80009a3d670089a22f
David Cárdenas <santito500@live.com>
2024-04-24 02:32:32 -0500

    Cambio en una ruta

--- routes/routes.js
+++ routes/routes.js
@@ -17,7 +17,7 @@ cardsRoutes.get("/cards", cardsController.filterCards);
 cardsRoutes.post("/getCardsByIDs", cardsController.getAll); // Devuelve los atibutos de las catas por los IDs ingresados //*(Requerido en miBanco y carritoDeCompra)
 cardsRoutes.post("/add-cards", cardsController.addBankCard); // Agrega cartas a la base de datos de invetario //*(requerido en miBanco, carritoCompras y subasta)
 cardsRoutes.post("/getBankCards", cardsController.getBankCard); // Devuelve todas las cartas del usuario //*(requerido en miBanco)
-cardsRoutes.post("/getCardsbyID", cardsController.getCardsbyID);
+//cardsRoutes.post("/getCardsbyID", cardsController.getCardsbyID);
 cardsRoutes.post("/deckCard", cardsController.addDeckCard);
 cardsRoutes.post("/getDeckCard", cardsController.getDeckCard);
 cardsRoutes.delete("/delete-card", cardsController.deleteBankCard); // Elimina las cartas de mi banco
